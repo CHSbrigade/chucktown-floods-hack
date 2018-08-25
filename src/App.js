efb2a0208fa37d9da21854483e9eeea7473294bf
Alex Boquist <aplchian@Alexs-MacBook-Pro-2.local>
2018-08-25 10:24:29 -0400

    added single quotes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react"
-import { Route, Switch } from "react-router"
-import { BrowserRouter } from "react-router-dom"
-import Button from "@material-ui/core/Button"
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
+import Button from '@material-ui/core/Button'
 
-import Map from "./components/Map"
+import Map from './components/Map'
 
 const fooComp = str => () => <div>{str}</div>
 
@@ -13,7 +13,7 @@ class App extends Component {
       <div className="App">
         <BrowserRouter>
           <Switch>
-            <Route exact path="/" component={fooComp("Main Page")} />
+            <Route exact path="/" component={fooComp('Main Page')} />
             <Route exact path="/map" component={Map} />
           </Switch>
         </BrowserRouter>
