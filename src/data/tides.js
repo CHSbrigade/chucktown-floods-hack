c5418bb4e8bcee99e06a7192aa27e41bfd2bbf47
Travis Nesland <tnesland@gmail.com>
2018-08-26 04:49:36 -0400

    Fetch tide data

---
+++ src/data/tides.js
@@ -0,0 +1,45 @@
+import fetch from 'isomorphic-fetch'
+import { DateTime } from 'luxon'
+import { compose, join, map, toPairs } from 'ramda'
+
+export default ({start, end} = {}) => {
+  const url = `${config.baseUrl}?${queryParams({
+    ...config.defaultParams,
+    begin_date: formatTs(beginTs(start)),
+    end_date: formatTs(endTs(end))
+  })}`
+
+  return fetch(url)
+    .then(res => res.json())
+    .then(json => JSON.stringify(json))
+}
+
+const config = {
+  baseUrl: 'https://tidesandcurrents.noaa.gov/api/datagetter',
+  defaultParams: {
+    datum: 'MTL',
+    format: 'json',
+    product: 'water_level',
+    station: '8665530',
+    time_zone: 'lst',
+    units: 'english',
+  }
+}
+
+const queryParams = compose(
+  encodeURI,
+  join('&'),
+  map(([k,v]) => `${k}=${v}`),
+  toPairs,
+)
+
+const formatTs = ts => ts.toFormat('yyyyMMdd HH:mm')
+
+const beginTs = ts => ts ?
+  DateTime(ts) :
+  DateTime.local().minus({ hours: 5 })
+
+const endTs = ts => ts ?
+  DateTime(ts) :
+  DateTime.local()
+
