f4d89c3fef4427fd49b7fed6514a1964584b0a65
Travis Nesland <tnesland@gmail.com>
2018-09-07 22:42:28 -0400

    Map: Remove applyOpacity logic
    This was for demonstration purposes only and it was causing rendering issues.

--- src/components/Map/components/Map.js
+++ src/components/Map/components/Map.js
@@ -1,39 +1,20 @@
 import React from "react"
 import ReactMapGL from "react-map-gl"
-import { forEach } from "ramda"
 
 import "mapbox-gl/dist/mapbox-gl.css"
 
 export default class extends React.Component {
-  static defaultProps = {
-    opacity: 0.5
-  }
-
-  constructor () {
-    super()
-    this.applyOpacity = this.applyOpacity.bind(this)
-  }
-
   render () {
-    const { opacity, ...restProps } = this.props
+    const { updateViewport, ...restProps } = this.props
 
     return(
       <ReactMapGL
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        onLoad={this.applyOpacity}
         onError={console.error}
-        onViewportCange={this.props.updateViewport}
+        onViewportCange={updateViewport}
         {...restProps}
         ref={el => this.mapRef = el}
       />
     )
-  }
-
-  applyOpacity () {
-    const { opacity } = this.props
-    const layerIds = ['flood 1', 'flood 2', 'flood 3']
-    const mapEl = this.mapRef.getMap()
-
-    forEach(id => mapEl.setPaintProperty(id, 'opacity', opacity), layerIds)
   }
 }
