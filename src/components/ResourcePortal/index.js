69370015d4e48ed084fa4320ebaa8d48f64f2c40
Travis Nesland <tnesland@gmail.com>
2018-09-08 01:46:42 -0400

    :sparkles: eslint fixes: ResourcePortal

--- src/components/ResourcePortal/index.js
+++ src/components/ResourcePortal/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import DataProvider from './components/DataProvider'
 import MainView from './components/MainView'
 
-export default extraProps => (
+const ResourcePortal = extraProps => (
   <DataProvider render={data => <MainView {...data} {...extraProps} />} />
 )
+
+ResourcePortal.displayName = 'ResourcePortal'
+
+export default ResourcePortal
